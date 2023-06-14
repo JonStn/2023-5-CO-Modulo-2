45dea2913d44920744d53b8b93d81779d8f2c8ae
jsmc <cardonastiven94@gmail.com>
2023-06-14 17:26:31 -0500

    Second - Class - Homework

--- game/components/enemies/enemy.py
+++ game/components/enemies/enemy.py
@@ -1,29 +1,24 @@
 import pygame
 import random
-
 from pygame.sprite import Sprite
-from game.utils.constants import ENEMY_1, SCREEN_HEIGHT, SCREEN_WIDTH
+from game.utils.constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 class Enemy(Sprite):
-    ENEMY_WIDTH = 40
-    ENEMY_HEIGHT = 60
-    X_POS_LIST = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550]
     Y_POS = 20
-    SPEED_X = 5
-    SPEED_Y = 4
     MOV_X = {0: 'left', 1: 'right'}
 
-    def __init__(self):
-        self.image = pygame.transform.scale(ENEMY_1, (self.ENEMY_WIDTH, self.ENEMY_HEIGHT))
+    def __init__(self, IMAGE, ENEMY_WIDTH, ENEMY_HEIGHT, SPEED_X, SPEED_Y, MIN_MOVE, MAX_MOVE):
+        self.image = pygame.transform.scale(IMAGE, (ENEMY_WIDTH,ENEMY_HEIGHT))
         self.rect = self.image.get_rect()
-        self.rect.x = self.X_POS_LIST[random.randint(0, 10)]
+        self.rect.x = random.randint(0, SCREEN_WIDTH - ENEMY_WIDTH)
         self.rect.y = self.Y_POS
-        self.speed_x = self.SPEED_X
-        self.speed_y = self.SPEED_Y
+        self.speed_x = SPEED_X
+        self.speed_y = SPEED_Y
         self.movement_x = self.MOV_X[random.randint(0, 1)]
-        self.move_x_for = random.randint(30, 100)
+        self.move_x_for = random.randint(MIN_MOVE, MAX_MOVE)
         self.index = 0
-
+    
+    
     def update(self, ships):
         self.rect.y += self.speed_y
 
@@ -43,9 +38,10 @@ class Enemy(Sprite):
 
     def change_movement_x(self):
         self.index += 1
-        if (self.index >= self.move_x_for and self.movement_x == 'right') or (self.rect.x >= SCREEN_WIDTH - self.ENEMY_WIDTH):
-            self.movement_x= 'left'
+        if (self.index >= self.move_x_for and self.movement_x == 'right') or (self.rect.x >= SCREEN_WIDTH - self.rect.width):
+            self.movement_x = 'left'
             self.index = 0
         elif (self.index >= self.move_x_for and self.movement_x == 'left') or (self.rect.x <= 10):
             self.movement_x = 'right'
             self.index = 0
+    
--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,3 +1,7 @@
+import pygame
+import random
+
+from game.utils.constants import ENEMY_1, ENEMY_2
 from game.components.enemies.enemy import Enemy
 
 class EnemyManager:
@@ -15,5 +19,9 @@ class EnemyManager:
 
     def add_enemy(self):
         if len(self.enemies) < 1:
-            enemy = Enemy()
-            self.enemies.append(enemy) 
+            enemy_type = random.choice([ENEMY_1, ENEMY_2])
+            if enemy_type == ENEMY_1:
+                enemy = Enemy(ENEMY_1, 40, 60, 3, 5, 30, 100)
+            elif enemy_type == ENEMY_2:
+                enemy = Enemy(ENEMY_2, 30, 40, 10 , 3, 5, 50)
+            self.enemies.append(enemy)
